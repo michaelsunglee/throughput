322b1b34ea3d5b51b4eeae8db91dc289ade80903
Michael Lee <michaelsunglee@gmail.com>
2018-05-24 22:43:28 -0400

    Search artist and redirect to show artist page.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,9 +1,39 @@
 class ArtistsController < ApplicationController
   def index
+    @image = session[:artist_image_url]
   end
 
-  def find_artist
+  def new
+  end
+
+  def create
+    @image = show_artists
+    # render template: "/artists/index.html.erb"
+    respond_to do |format|
+      if @image
+        session[:artist_image_url] = @image
+        format.html { redirect_to action: :index }
+        # format.json { render :index }
+      end
+    end
+  end
+
+  def show_artists
+    authenticate_rspotify_client
     artist = params[:artist]
-    puts "artist is #{artist}"
+    main = RSpotify::Artist.search(artist).first
+    get_image_url(main.images)
+  end
+
+  private
+
+  def authenticate_rspotify_client
+    RSpotify::authenticate(Rails.application.secrets.rspotify_client,
+                          Rails.application.secrets.rspotify_secret)
+  end
+
+  def get_image_url(images)
+    # The Spotify images array will have 3 pre-determined image sizes or none
+    images.second['url'] || ''
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :searches
-  resources :artists, only: [:index]
-  get 'find_artist', to: 'artists#find_artist'
-  get 'get_artist_from_query', to: 'searches#get_artist', remote: true
-  root to: 'artists#index'
+  resources :artists, only: [:index, :create, :new]
+  root to: 'artists#new'
 end
